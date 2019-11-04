b84d3d3788c54c404f589b003b66fff53b12ac8c
Samuel Collet <samuelpcollet@gmail.com>
2019-11-03 20:20:47 -0600

    first push

---
+++ source/main.c
@@ -0,0 +1,15 @@
+int main(void) {
+	DarkEngine *engine;
+	
+	engine = DarkEngine("", "");
+
+	/*
+	init engine
+	start main loop...
+	when to submit all objs to load?
+	load_scene("");
+	on_mouse_click();
+	*/	
+
+	return (0);
+}
